debce0c6cccf98819fdf8389003f1f34200c88d2
ArturVargas <arturvargas9226@gmail.com>
2019-08-21 12:27:43 -0500

    [add] create & read post controller

--- 00_RepasoExpress/controllers/post.js
+++ 00_RepasoExpress/controllers/post.js
@@ -1,13 +1,38 @@
 
+const Post = require('../models/Post');
+
 exports.newPost = (req, res) => {
-    res.json('Nuevo Post Creado')
+    const params = req.body;
+    if(params.title && params.body){
+        let newPost = Post({
+            ...params
+        })
+        newPost.save((err, post) => {
+            if(err){
+                res.status(500).json({message: err})
+            }
+            res.status(201).json({data: post})
+        })
+    } else {
+        res.status(400).json({message: 'Los datos son requeridos'})
+    }
 }
 
 exports.listPosts = (req, res) => {
-    res.json('Lista de Posts')
+    Post.find().exec((err, posts) => {
+        if(err){
+            res.status(500).json({message: err})
+        } 
+        res.status(200).json({data: posts})
+    })
 }
 
 exports.post = (req, res) => {
     let id = req.params.id;
-    res.json(`Post ${id}`)
+    Post.findById(id).exec((err, post) => {
+        if(err){
+            res.status(500).json({message: err})
+        }
+        res.status(200).json({data: post})
+    })
 }
